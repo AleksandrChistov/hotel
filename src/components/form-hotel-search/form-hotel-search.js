0abd9f5b4f3b71013f22415929d35c984174bd4f
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-11-20 17:56:04 +0700

    Сверстал 4 формы в разделе Cards на странице UI kit

--- src/components/form-hotel-search/form-hotel-search.js
+++
@@ -1,9 +0,0 @@
-import 'air-datepicker'
-
-$('.datepicker-here').datepicker({
-  // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
-  minDate: new Date()
-})
-
-// Доступ к экземпляру объекта
-$('.datepicker-here').data('datepicker')
